ab6a758a23473e50a9258174bdc21350402bd1a0
ajulkjose246 <mail.ajulkjose@gmail.com>
2023-03-10 20:05:28 +0530

    offering payment page added

--- pages/payment.php
+++ pages/payment.php
@@ -1,573 +1,92 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>FormWizard_v1</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="author" content="colorlib.com">
+<html lang="en">
+<?php
+require("../connection/db_connect.php");
+$offer_id = $_GET['id'];
+$sql = "SELECT * FROM `tbl_offering` WHERE `offer_id` =$offer_id";
+$result = mysqli_query($con, $sql);
+$row = mysqli_fetch_array($result);
+?>
 
-		<!-- MATERIAL DESIGN ICONIC FONT -->
-		<link rel="stylesheet" href="fonts/material-design-iconic-font/css/material-design-iconic-font.css">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Payment</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+  <link rel="stylesheet" href="/parmas/assets/css/payment.css">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+</head>
 
-		<!-- STYLE CSS -->
-		<link rel="stylesheet" href="css/style.css">
-        <style>
-            @font-face {
-  font-family: "Muli-Regular";
-  src: url("../fonts/muli/Muli-Regular.ttf"); }
-@font-face {
-  font-family: "Muli-SemiBold";
-  src: url("../fonts/muli/Muli-SemiBold.ttf"); }
-* {
-  -webkit-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  box-sizing: border-box; }
+<body>
+  <section>
+    <div class="container bg-light d-md-flex align-items-center">
+      <div class="card box1 shadow-sm p-md-5 p-md-5 p-4">
+        <div class="fw-bolder mb-4"></span><span class="ps-1"><?= $row['offer_name'] ?></span></div>
+        <div class="fw-bolder mb-4"></span><span class="ps-1">(<?= $row['offer_name_mala'] ?>)</span></div>
+        <div class="fw-bolder mb-4"><span class="fas fa-rupee-sign"></span><span class="ps-1"><?= $row['offer_price'] ?></span></div>
+        <div class="d-flex flex-column">
+          <div class="d-flex flex-column mb-5"> <span class="far fa-calendar-alt text"><span class="ps-2">Payment Date :</span></span> <span class="ps-3"><?= date("d/m/Y") ?></span> </div>
+          <div class="d-flex align-items-center justify-content-between text mt-5">
+          </div>
+        </div>
+      </div>
+      <div class="card box2 shadow-sm">
+        <div class="d-flex align-items-center justify-content-between p-md-5 p-4"> <span class="h5 fw-bold m-0">Payment methods</span>
+        </div>
+        <ul class="nav nav-tabs mb-3 px-md-4 px-2">
+          <li class="nav-item"> <a class="nav-link px-2 active" aria-current="page" href="#">Enter Details</a> </li>
+        </ul>
 
-body {
-  font-family: "Muli-Regular";
-  font-size: 15px;
-  color: #999;
-  margin: 0; }
+        <form action="">
+          <div class="row">
+            <div class="col-12">
+              <div class="d-flex flex-column px-md-5 px-4 mb-4"> <span>Email</span>
+                <div class="inputWithIcon"> <input class="form-control" type="text"></div>
+              </div>
+            </div>
+            <div class="col-md-11">
+              <div class="d-flex flex-column ps-md-5 px-md-0 px-4 mb-4"> <span>Offering<span class="ps-1">Date</span></span>
+                <div class="inputWithIcon"> <input type="text" class="form-control" id="datepicker"></div>
+              </div>
+            </div>
 
-:focus {
-  outline: none; }
-
-textarea {
-  resize: none; }
-
-input, textarea, select, button {
-  font-family: "Muli-Regular";
-  font-size: 15px; }
-
-p, h1, h2, h3, h4, h5, h6, ul {
-  margin: 0; }
-
-ul {
-  padding: 0;
-  margin: 0;
-  list-style: none; }
-
-a {
-  text-decoration: none; }
-
-textarea {
-  resize: none; }
-
-img {
-  max-width: 100%; }
-
-.wrapper {
-  height: 100vh;
-  background: #f9f6f1;
-  display: flex;
-  align-items: center;
-  justify-content: center; }
-
-.wizard {
-  width: 920px;
-  position: relative; }
-
-.wizard > .steps {
-  position: absolute;
-  top: 27.44%;
-  right: 30%;
-  transform: translateX(50%);
-  z-index: 9; }
-
-.wizard > .steps .current-info, .wizard > .steps .number {
-  display: none; }
-
-.wizard > .steps ul {
-  display: flex;
-  justify-content: center; }
-
-.wizard > .steps li a {
-  display: inline-block;
-  width: 12px;
-  height: 12px;
-  border-radius: 50%;
-  background: #e9e0cf;
-  margin-right: 78px;
-  position: relative; }
-  .wizard > .steps li a:before {
-    content: "";
-    width: 58px;
-    height: 2px;
-    background: #e9e0cf;
-    position: absolute;
-    right: 22px;
-    top: 5px; }
-  .wizard > .steps li a:after {
-    content: "";
-    width: 0;
-    height: 2px;
-    background: #6d7f52;
-    position: absolute;
-    left: -68px;
-    top: 5px;
-    transition: all 0.6s ease; }
-.wizard > .steps li.first a {
-  background: #6d7f52; }
-.wizard > .steps li.checked a {
-  background: #6d7f52; }
-  .wizard > .steps li.checked a:after {
-    width: 58px; }
-.wizard > .steps li:last-child a {
-  margin-right: 0; }
-.wizard > .steps li:first-child a:before {
-  display: none; }
-.wizard > .steps li:first-child a:after {
-  display: none; }
-
-.wizard > .content {
-  position: relative;
-  height: 521px; }
-
-.inner {
-  display: flex;
-  background: #fff;
-  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.2);
-  -webkit-box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.2);
-  -moz-box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.2);
-  -ms-box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.2);
-  -o-box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.2); }
-
-.image-holder {
-  width: 40%;
-  height: 521px; }
-
-.form-content {
-  width: 60%;
-  padding-top: 65px;
-  padding-left: 51px;
-  padding-right: 51px; }
-
-.form-header {
-  text-align: center; }
-
-h3 {
-  font-family: "Muli-SemiBold";
-  font-size: 22px;
-  color: #453e79;
-  text-transform: uppercase;
-  letter-spacing: 7px;
-  padding: 5px 15px;
-  display: inline-block;
-  border: 1px solid #dad8e4;
-  margin-bottom: 75px; }
-
-p {
-  font-family: "Muli-SemiBold";
-  font-size: 20px;
-  color: #6d7f52;
-  text-align: center;
-  margin-bottom: 26px; }
-
-.form-row {
-  display: flex;
-  margin-bottom: 18px; }
-  .form-row .form-holder {
-    width: 50%;
-    margin-right: 30px; }
-    .form-row .form-holder:last-child {
-      margin-right: 0; }
-    .form-row .form-holder.w-100 {
-      width: 100%;
-      margin-right: 0; }
-  .form-row .select {
-    width: 50%;
-    margin-right: 30px; }
-    .form-row .select .form-holder {
-      width: 100%;
-      margin-right: 0; }
-
-.select {
-  position: relative; }
-  .select .select-control {
-    height: 37px;
-    border-bottom: 2px solid #e6e6e6;
-    width: 100%;
-    color: #999;
-    display: flex;
-    align-items: center;
-    cursor: pointer; }
-  .select .dropdown {
-    display: none;
-    position: absolute;
-    top: 100%;
-    width: 100%;
-    background: #fff;
-    z-index: 9;
-    border: 1px solid #81acee; }
-    .select .dropdown li {
-      padding: 5px 10px; }
-      .select .dropdown li:hover {
-        background: #81acee;
-        color: #fff; }
-
-.form-holder {
-  position: relative; }
-  .form-holder i {
-    position: absolute;
-    bottom: 7px;
-    right: 0;
-    font-size: 17px; }
-
-.form-control {
-  height: 37px;
-  border: none;
-  border-bottom: 2px solid #e6e6e6;
-  width: 100%;
-  color: #666; }
-  .form-control:focus {
-    border-color: #e9e0cf; }
-  .form-control::-webkit-input-placeholder {
-    color: #999; }
-  .form-control::-moz-placeholder {
-    color: #999; }
-  .form-control:-ms-input-placeholder {
-    color: #999; }
-  .form-control:-moz-placeholder {
-    color: #999; }
-
-select.form-control {
-  -moz-appearance: none;
-  -webkit-appearance: none;
-  cursor: pointer;
-  color: #999; }
-  select.form-control option[value=""][disabled] {
-    display: none; }
-
-textarea.form-control {
-  padding: 8px 0;
-  background: url("../images/textarea-bg.png") no-repeat right bottom 3px; }
-
-.actions {
-  position: absolute;
-  bottom: 55px;
-  right: 51px; }
-  .actions ul {
-    display: flex; }
-  .actions li a {
-    padding: 0;
-    border: none;
-    display: inline-flex;
-    height: 41px;
-    width: 135px;
-    align-items: center;
-    background: #e9e0cf;
-    font-family: "Muli-SemiBold";
-    color: #666;
-    cursor: pointer;
-    position: relative;
-    padding-left: 28px; }
-    .actions li a:before {
-      content: '\f301';
-      position: absolute;
-      top: 14px;
-      right: 28px;
-      font-family: Material-Design-Iconic-Font; }
-    .actions li a:hover {
-      background: #cfc7b8; }
-  .actions li:first-child a {
-    width: 144px;
-    padding-left: 48px; }
-    .actions li:first-child a:before {
-      content: '\f2ff';
-      position: absolute;
-      top: 14px;
-      left: 26px;
-      font-family: Material-Design-Iconic-Font; }
-  .actions li:nth-child(2) a, .actions li:last-child a {
-    margin-left: 20px; }
-  .actions li:last-child a {
-    width: 124px;
-    background: #6d7f52;
-    color: #fff; }
-    .actions li:last-child a:hover {
-      background: #849963; }
-    .actions li:last-child a:before {
-      content: '\f26b';
-      position: absolute;
-      top: 13px;
-      right: 28px;
-      font-family: Material-Design-Iconic-Font; }
-  .actions li[aria-disabled="true"] a {
-    opacity: 0;
-    transition: all 1s; }
-  .actions li[aria-disabled="false"] ~ li a {
-    background: #6d7f52;
-    color: #fff; }
-    .actions li[aria-disabled="false"] ~ li a:hover {
-      background: #849963; }
-
-.checkbox-tick label {
-  cursor: pointer;
-  display: inline-block;
-  padding-left: 23px;
-  position: relative; }
-  .checkbox-tick label.male {
-    margin-right: 26px; }
-.checkbox-tick input {
-  position: absolute;
-  opacity: 0;
-  cursor: pointer; }
-.checkbox-tick input:checked ~ .checkmark {
-  background: #999; }
-  .checkbox-tick input:checked ~ .checkmark:after {
-    display: block; }
-.checkbox-tick .checkmark {
-  position: absolute;
-  top: 3px;
-  left: 0;
-  height: 13px;
-  width: 13px;
-  border: 1px solid #999;
-  border-radius: 50%;
-  font-family: Material-Design-Iconic-Font;
-  color: #fff;
-  font-size: 11px; }
-  .checkbox-tick .checkmark:after {
-    top: 0;
-    left: 2px;
-    position: absolute;
-    display: none;
-    content: '\f26b'; }
-
-.checkbox-circle {
-  position: relative;
-  padding-left: 23px;
-  margin-top: 41px;
-  width: 63%; }
-  .checkbox-circle.mt-24 {
-    margin-top: 24px; }
-  .checkbox-circle label {
-    cursor: pointer;
-    color: #999;
-    font-size: 13px;
-    line-height: 1.9;
-    transform: translateY(-5px); }
-    .checkbox-circle label a {
-      color: #6d7f52; }
-      .checkbox-circle label a:hover {
-        color: #89b843; }
-  .checkbox-circle input {
-    position: absolute;
-    opacity: 0;
-    cursor: pointer; }
-  .checkbox-circle input:checked ~ .checkmark:after {
-    display: block; }
-  .checkbox-circle .checkmark {
-    position: absolute;
-    top: 6px;
-    left: 0;
-    height: 13px;
-    width: 13px;
-    border-radius: 50%;
-    border: 1px solid #999; }
-    .checkbox-circle .checkmark:after {
-      content: "";
-      top: 2px;
-      left: 2px;
-      width: 7px;
-      height: 7px;
-      border-radius: 50%;
-      background: #999;
-      position: absolute;
-      display: none; }
-
-@media (max-width: 991px) {
-  .wizard {
-    max-width: 768px; }
-
-  .image-holder img {
-    height: 521px;
-    object-fit: cover; } }
-@media (max-width: 767px) {
-  .inner {
-    display: block;
-    box-shadow: none;
-    -webkit-box-shadow: none;
-    -moz-box-shadow: none;
-    -ms-box-shadow: none;
-    -o-box-shadow: none; }
-
-  .image-holder {
-    width: 100%; }
-    .image-holder img {
-      width: 100%; }
-
-  .form-content {
-    width: 100%;
-    padding: 50px 20px 30px; }
-
-  .form-row {
-    display: block; }
-    .form-row .form-holder, .form-row .select {
-      width: 100%;
-      margin-right: 0;
-      margin-bottom: 18px; }
-
-  .wrapper {
-    height: 1201px;
-    background: none; }
-
-  .checkbox-circle {
-    width: 100%; }
-
-  .wizard {
-    height: 100%; }
-    .wizard > .steps {
-      right: 50%;
-      top: 52%;
-      transform: translateX(50%); }
-
-  .actions {
-    right: 50%;
-    transform: translateX(50%);
-    bottom: 3%; } }
-
-/*# sourceMappingURL=style.css.map */
-
-        </style>
-	</head>
-	<body>
-		<div class="wrapper">
-            <form action="" id="wizard">
-        		<!-- SECTION 1 -->
-                <h2></h2>
-                <section>
-                    <div class="inner">
-						<div class="image-holder">
-							<img src="images/form-wizard-1.jpg" alt="">
-						</div>
-						<div class="form-content" >
-							<div class="form-header">
-								<h3>Registration</h3>
-							</div>
-							<p>Please fill with your details</p>
-							<div class="form-row">
-								<div class="form-holder">
-									<input type="text" placeholder="First Name" class="form-control">
-								</div>
-								<div class="form-holder">
-									<input type="text" placeholder="Last Name" class="form-control">
-								</div>
-							</div>
-							<div class="form-row">
-								<div class="form-holder">
-									<input type="text" placeholder="Your Email" class="form-control">
-								</div>
-								<div class="form-holder">
-									<input type="text" placeholder="Phone Number" class="form-control">
-								</div>
-							</div>
-							<div class="form-row">
-								<div class="form-holder">
-									<input type="text" placeholder="Age" class="form-control">
-								</div>
-								<div class="form-holder" style="align-self: flex-end; transform: translateY(4px);">
-									<div class="checkbox-tick">
-										<label class="male">
-											<input type="radio" name="gender" value="male" checked> Male<br>
-											<span class="checkmark"></span>
-										</label>
-										<label class="female">
-											<input type="radio" name="gender" value="female"> Female<br>
-											<span class="checkmark"></span>
-										</label>
-									</div>
-								</div>
-							</div>
-							<div class="checkbox-circle">
-								<label>
-									<input type="checkbox" checked> Nor again is there anyone who loves or pursues or desires to obtaini.
-									<span class="checkmark"></span>
-								</label>
-							</div>
-						</div>
-					</div>
-                </section>
-
-				<!-- SECTION 2 -->
-                <h2></h2>
-                <section>
-                    <div class="inner">
-						<div class="image-holder">
-							<img src="images/form-wizard-2.jpg" alt="">
-						</div>
-						<div class="form-content">
-							<div class="form-header">
-								<h3>Registration</h3>
-							</div>
-							<p>Please fill with additional info</p>
-							<div class="form-row">
-								<div class="form-holder w-100">
-									<input type="text" placeholder="Address" class="form-control">
-								</div>
-							</div>
-							<div class="form-row">
-								<div class="form-holder">
-									<input type="text" placeholder="City" class="form-control">
-								</div>
-								<div class="form-holder">
-									<input type="text" placeholder="Zip Code" class="form-control">
-								</div>
-							</div>
-
-							<div class="form-row">
-								<div class="select">
-									<div class="form-holder">
-										<div class="select-control">Your country</div>
-										<i class="zmdi zmdi-caret-down"></i>
-									</div>
-									<ul class="dropdown">
-										<li rel="United States">United States</li>
-										<li rel="United Kingdom">United Kingdom</li>
-										<li rel="Viet Nam">Viet Nam</li>
-									</ul>
-								</div>
-								<div class="form-holder"></div>
-							</div>
-						</div>
-					</div>
-                </section>
-
-                <!-- SECTION 3 -->
-                <h2></h2>
-                <section>
-                    <div class="inner">
-						<div class="image-holder">
-							<img src="images/form-wizard-3.jpg" alt="">
-						</div>
-						<div class="form-content">
-							<div class="form-header">
-								<h3>Registration</h3>
-							</div>
-							<p>Send an optional message</p>
-							<div class="form-row">
-								<div class="form-holder w-100">
-									<textarea name="" id="" placeholder="Your messagere here!" class="form-control" style="height: 99px;"></textarea>
-								</div>
-							</div>
-							<div class="checkbox-circle mt-24">
-								<label>
-									<input type="checkbox" checked>  Please accept <a href="#">terms and conditions ?</a>
-									<span class="checkmark"></span>
-								</label>
-							</div>
-						</div>
-					</div>
-                </section>
-            </form>
-		</div>
-
-		<!-- JQUERY -->
-		<script src="js/jquery-3.3.1.min.js"></script>
-
-		<!-- JQUERY STEP -->
-		<script src="js/jquery.steps.js"></script>
-		<script src="js/main.js"></script>
-		<!-- Template created and distributed by Colorlib -->
+            <div class="col-12">
+              <div class="d-flex flex-column px-md-5 px-4 mb-4"> <span>Please List out all your Holy Mass Intentions One by One</span>
+                <div class="inputWithIcon"> <textarea class="form-control" rows="2"></textarea></div>
+              </div>
+            </div>
+            <div class="col-12 px-md-5 px-4 mt-3">
+              <div class="btn btn-primary w-100">Pay ₹<?= $row['offer_price'] ?></div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
 </body>
+<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+<script>
+  $(function() {
+
+    var unavailableDates = ["9-3-2023", "14-3-2023", "15-3-2023"];
+
+    function unavailable(date) {
+      dmy = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+      if ($.inArray(dmy, unavailableDates) == -1) {
+        return [true, ""];
+      } else {
+        return [false, "", "Unavailable"];
+      }
+    }
+
+    $("#datepicker").datepicker({
+      dateFormat: 'dd/mm/yy',
+      beforeShowDay: unavailable
+    });
+  });
+</script>
+
 </html>
